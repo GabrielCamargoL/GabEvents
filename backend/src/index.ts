ab40647e0794334210bfd7e8a103cd2827ebec13
GabrielCamargoL <gabriel.cleite@outlook.com>
2022-09-06 00:10:35 -0300

    use Cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 3333;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(routes);
 
 app.listen(PORT, () => console.log(`🔥 Server started at http://localhost:${PORT}`));
