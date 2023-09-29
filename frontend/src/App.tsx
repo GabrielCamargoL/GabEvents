40cd9831314cb56285f58d4bc0223900b00a5d37
GabrielCamargoL <gabriel.cleite@outlook.com>
2023-09-29 09:30:07 -0300

    change AWS to Render in index.ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router";
-import aws from "./services/aws";
+import api from "./services/api";
 
 export function App() {
 
   useEffect(() => {
     setInterval(() => {
-      aws.get('/').then(res => {
+      api.get('/').then(res => {
         alert(res.data.message)
         console.log(res)
       })
